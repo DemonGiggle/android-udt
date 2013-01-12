daf678563c2a27ee0ded946f80cf767d5507fd5a
giggle <giggle@zillians.com>
2013-01-12 15:41:44 +0800

    add rest app

--- jni/Android.mk
+++ jni/Android.mk
@@ -13,7 +13,32 @@ LOCAL_MODULE    := appclient
 LOCAL_SRC_FILES := app/appclient.cpp 
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/lib 
 LOCAL_SHARED_LIBRARIES := udt
-#LOCAL_EXPORT_LDLIBS += -L$(OBJS_DIR) -ludt
 include $(BUILD_EXECUTABLE)
 
-#app/appserver.cpp app/recvfile.cpp app/sendfile.cpp app/test.cpp
+include $(CLEAR_VARS)
+LOCAL_MODULE    := appserver
+LOCAL_SRC_FILES := app/appserver.cpp
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/lib 
+LOCAL_SHARED_LIBRARIES := udt
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := recvfile
+LOCAL_SRC_FILES := app/recvfile.cpp
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/lib 
+LOCAL_SHARED_LIBRARIES := udt
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := sendfile
+LOCAL_SRC_FILES := app/sendfile.cpp
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/lib 
+LOCAL_SHARED_LIBRARIES := udt
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := test
+LOCAL_SRC_FILES := app/test.cpp
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/lib 
+LOCAL_SHARED_LIBRARIES := udt
+include $(BUILD_EXECUTABLE)
