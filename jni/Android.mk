883cabd1a29d242a617c2c7ae51f00d7d9383c66
giggle <giggle@zillians.com>
2013-01-12 15:33:46 +0800

    incorporate test application

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,19 +2,18 @@ LOCAL_PATH := $(call my-dir)
 LOCAL_CPP_EXTENSION:=.cpp 
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE    := udt
-LOCAL_SRC_FILES := md5.cpp common.cpp window.cpp list.cpp buffer.cpp packet.cpp channel.cpp queue.cpp ccc.cpp cache.cpp core.cpp epoll.cpp api.cpp 
-
-#LOCAL_CFLAGS += -I/usr/include/c++/4.7.1 
-#LOCAL_CFLAGS += -I/usr/include/4.7.1/
-
-#LOCAL_C_INCLUDES += /usr/include/c++/4.7.1
-#LOCAL_C_INCLUDES += /usr/include/c++/4.7.1/x86_64-unknown-linux-gnu
-
+LOCAL_SRC_FILES := lib/md5.cpp lib/common.cpp lib/window.cpp lib/list.cpp lib/buffer.cpp lib/packet.cpp lib/channel.cpp lib/queue.cpp lib/ccc.cpp lib/cache.cpp lib/core.cpp lib/epoll.cpp lib/api.cpp 
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/lib
 LOCAL_CFLAGS += -fexceptions
-
-#LOCAL_C_INCLUDES += /opt2/adt-bundle-linux-x86_64/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.7/include
-#LOCAL_C_INCLUDES += /opt2/adt-bundle-linux-x86_64/android-ndk-r8d/sources/cxx-stl/gnu-libstdc++/4.7/libs/armeabi/include/
-
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := appclient
+LOCAL_SRC_FILES := app/appclient.cpp 
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/lib 
+LOCAL_SHARED_LIBRARIES := udt
+#LOCAL_EXPORT_LDLIBS += -L$(OBJS_DIR) -ludt
+include $(BUILD_EXECUTABLE)
+
+#app/appserver.cpp app/recvfile.cpp app/sendfile.cpp app/test.cpp
